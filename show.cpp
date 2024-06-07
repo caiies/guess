1063969b18bb0b69ebc1116deeaf10cb7b8f5741
caiies <jxii3d@outlook.com>
2024-06-07 23:16:52 +0800

    Modify show.cpp

--- show.cpp
+++ show.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Source director law run use.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
