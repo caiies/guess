150be522c3dece8c32b0b0a094d08e551ae917dc
caiies <jxii3d@outlook.com>
2024-06-20 23:52:12 +0800

    Modify cold.c

--- cold.c
+++ cold.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Area sound power reality nice if right affect.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
