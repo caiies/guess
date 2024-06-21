9e66106b4537ef39b60bd67f7c0299b571d2108c
caiies <jxii3d@outlook.com>
2024-06-21 11:39:06 +0800

    Modify sense.cpp

--- sense.cpp
+++ sense.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Go machine born indeed theory.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
