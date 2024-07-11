67c3ae2b2446fba9274b468bf954bf4b730c7559
caiies <jxii3d@outlook.com>
2024-07-11 13:04:58 +0800

    Modify bit.cpp

--- bit.cpp
+++ bit.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Ten go stock season.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
