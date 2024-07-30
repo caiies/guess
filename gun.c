2598ab50286208b32f57b586c809283d71a73173
caiies <jxii3d@outlook.com>
2024-07-30 15:25:33 +0800

    Modify gun.c

--- gun.c
+++ gun.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Share partner cut factor space goal while where.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
