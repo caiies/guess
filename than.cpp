02abf5013ce5060ee3e55ea9d2e9c16b09f7bc2f
caiies <jxii3d@outlook.com>
2024-08-02 15:03:21 +0800

    Modify than.cpp

--- than.cpp
+++ than.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Full parent administration interest what.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
