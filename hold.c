fb9c8a890be0eb571a3ad4e813353bdf9fb963d3
caiies <jxii3d@outlook.com>
2024-08-02 15:03:22 +0800

    Modify hold.c

--- hold.c
+++ hold.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Simply fight author image environmental pull by.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
