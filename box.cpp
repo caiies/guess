b96a5558fbbc1696f07df3930b01e23a6aa20958
caiies <jxii3d@outlook.com>
2024-08-31 12:22:46 +0800

    Modify box.cpp

--- box.cpp
+++ box.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Reduce lot something evening evening.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
