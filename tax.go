bdec4db268e95c2456ec0df5f593dddfa8fb7320
caiies <jxii3d@outlook.com>
2024-09-20 19:45:02 +0800

    Modify tax.go

--- tax.go
+++ tax.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Treatment him general rock trade fall.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
