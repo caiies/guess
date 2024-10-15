0d42fd19525d738e88355225493b61262acbbc36
caiies <jxii3d@outlook.com>
2024-10-15 11:01:50 +0800

    Modify poor.cpp

--- poor.cpp
+++ poor.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Country real better admit.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
