f77e3b3297b112fd8c6ca47a7e9ca02355b22940
caiies <jxii3d@outlook.com>
2024-11-22 13:41:06 +0800

    Modify agent.cpp

--- agent.cpp
+++ agent.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Gun bag other determine need.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
