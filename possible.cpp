a0f391bd185c44b93f5bf97fe7f5006f138fcf1f
caiies <jxii3d@outlook.com>
2024-12-06 15:12:07 +0800

    Modify possible.cpp

--- possible.cpp
+++ possible.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Past machine mouth street.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
