a9a5b7e5ca3e9e8f1a35ac3549f272e5caf4a6b5
caiies <jxii3d@outlook.com>
2025-02-23 15:26:25 +0800

    Modify apply.cpp

--- apply.cpp
+++ apply.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Economy with raise offer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
